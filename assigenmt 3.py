da75a868dda6a3220734751a62ab7d2a26c68457
shiimaamiri <140455365+shiimaamiri@users.noreply.github.com>
2023-08-05 01:07:42 +0330

    Add files via upload

---
+++ assigenmt 3.py
@@ -0,0 +1,21 @@
+import random
+start=input("if start game enter s :")
+if start == "s" : 
+    partab = 1 
+    while partab !=0 :
+      tas = random. randint(1,7)
+      print(tas)
+      if tas == 6 :
+        print ("you win , you have twe chance :")
+        partab = partab+1
+      else :
+        partab= partab-1
+                
+
+            
+
+        
+     
+         
+           
+
